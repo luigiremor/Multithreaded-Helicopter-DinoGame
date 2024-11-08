89502ea620f39d367a59cf0f0f43fa0f40761a74
luigi remor <luigiremor@gmail.com>
2024-11-07 21:42:22 -0300

    Refactor helicopter game to use classes for Helicopter and Missile, enhancing code organization and thread safety

--- game.cpp
+++ game.cpp
@@ -2,50 +2,143 @@
 #include <thread>
 #include <mutex>
 #include <atomic>
+#include <vector>
 #include <ncurses.h>
 #include <unistd.h>
 
-// Scene dimensions
+// Scenario dimensions
 const int WIDTH = 50;
 const int HEIGHT = 20;
 
-// Initial position of the helicopter
-std::atomic<int> heli_x(WIDTH / 2);
-std::atomic<int> heli_y(HEIGHT / 2);
+// Class to represent the helicopter
+class Helicopter
+{
+public:
+    std::atomic<int> x;
+    std::atomic<int> y;
+    std::atomic<int> remaining_missiles;
+    std::mutex mtx;
 
-std::mutex mtx;
+    Helicopter(int startX, int startY, int capacity)
+        : x(startX), y(startY), remaining_missiles(capacity) {}
+
+    void move(int dx, int dy)
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        x += dx;
+        y += dy;
+    }
+
+    bool can_fire()
+    {
+        return remaining_missiles > 0;
+    }
+
+    void fire()
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        if (remaining_missiles > 0)
+            remaining_missiles--;
+    }
+
+    void reload(int amount)
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        remaining_missiles += amount;
+    }
+};
+
+// Class to represent a missile
+class Missile
+{
+public:
+    int x;
+    int y;
+    bool active;
+    std::thread th;
+
+    Missile(int startX, int startY)
+        : x(startX), y(startY), active(true) {}
+
+    void start()
+    {
+        th = std::thread(&Missile::move, this);
+    }
+
+    void move()
+    {
+        while (active && x < WIDTH - 1)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            x++;
+        }
+        active = false;
+    }
+
+    void draw()
+    {
+        if (active)
+        {
+            mvprintw(y, x, "-");
+        }
+    }
+
+    void join()
+    {
+        if (th.joinable())
+            th.join();
+    }
+};
+
+// Global variables
+Helicopter heli(WIDTH / 2, HEIGHT / 2, 5); // Initial capacity of 5 missiles
+std::vector<Missile *> missiles;
+std::mutex mtx_missiles;
 std::atomic<bool> running(true);
 
+// Function to manage player input
 void thread_input()
 {
     int ch;
-    nodelay(stdscr, TRUE);
-    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE); // Does not block waiting for input
+    keypad(stdscr, TRUE);  // Captures special keys
     while (running)
     {
         ch = getch();
-        mtx.lock();
         switch (ch)
         {
         case KEY_UP:
         case 'w':
-            if (heli_y > 1)
-                heli_y--;
+            if (heli.y > 1)
+                heli.y--;
             break;
         case KEY_DOWN:
         case 's':
-            if (heli_y < HEIGHT - 2)
-                heli_y++;
+            if (heli.y < HEIGHT - 2)
+                heli.y++;
             break;
         case KEY_LEFT:
         case 'a':
-            if (heli_x > 1)
-                heli_x--;
+            if (heli.x > 1)
+                heli.x--;
             break;
         case KEY_RIGHT:
         case 'd':
-            if (heli_x < WIDTH - 2)
-                heli_x++;
+            if (heli.x < WIDTH - 2)
+                heli.x++;
+            break;
+        case ' ':
+            if (heli.can_fire())
+            {
+                heli.fire();
+                // Create and start a new missile
+                Missile *m = new Missile(heli.x + 1, heli.y);
+                {
+                    std::lock_guard<std::mutex> lock(mtx_missiles);
+                    missiles.push_back(m);
+                }
+                m->start();
+            }
             break;
         case 'q':
             running = false;
@@ -53,18 +146,17 @@ void thread_input()
         default:
             break;
         }
-        mtx.unlock();
-        usleep(10000);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
 
+// Function to render the scenario
 void thread_render()
 {
     while (running)
     {
-        mtx.lock();
         clear();
-
+        // Draw borders
         for (int i = 0; i < WIDTH; i++)
         {
             mvprintw(0, i, "#");
@@ -76,13 +168,37 @@ void thread_render()
             mvprintw(i, WIDTH - 1, "#");
         }
         // Draw helicopter
-        mvprintw(heli_y.load(), heli_x.load(), "H");
+        mvprintw(heli.y.load(), heli.x.load(), "H");
+
+        // Draw missiles
+        {
+            std::lock_guard<std::mutex> lock(mtx_missiles);
+            for (auto it = missiles.begin(); it != missiles.end();)
+            {
+                if ((*it)->active)
+                {
+                    (*it)->draw();
+                    ++it;
+                }
+                else
+                {
+                    // Join the thread and remove the missile from the list
+                    (*it)->join();
+                    delete *it;
+                    it = missiles.erase(it);
+                }
+            }
+        }
+
+        // Show remaining missiles
+        mvprintw(HEIGHT, 0, "Remaining missiles: %d", heli.remaining_missiles.load());
+
         refresh();
-        mtx.unlock();
-        usleep(50000);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 }
 
+// Main function
 int main()
 {
     // Initialize ncurses
@@ -98,8 +214,20 @@ int main()
     input_thread.join();
     render_thread.join();
 
-    // Finalize ncurses
+    // End ncurses
     endwin();
+
+    // Clear remaining missiles
+    {
+        std::lock_guard<std::mutex> lock(mtx_missiles);
+        for (auto m : missiles)
+        {
+            m->active = false;
+            m->join();
+            delete m;
+        }
+        missiles.clear();
+    }
 
     return 0;
 }
